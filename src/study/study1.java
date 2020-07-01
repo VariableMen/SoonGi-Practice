c0d7abab892d6a66e559b86600e2edd367590b9c
user <user@DESKTOP-D1QKU62>
2020-07-01 17:02:17 +0900

    study 파일

---
+++ src/study/study1.java
@@ -0,0 +1,17 @@
+package study;
+
+public class study1 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		int value = 100;
+		int num = 150;
+		double doub = 15.6;
+		
+		System.out.println(value + num);
+
+	}
+
+}
