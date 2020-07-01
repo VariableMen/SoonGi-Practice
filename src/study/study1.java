7ef0bd149899e4e99a7db3ece446bb3a57e696f1
user <user@DESKTOP-D1QKU62>
2020-07-01 17:45:31 +0900

    스터디 연습문제

--- src/study/study1.java
+++ src/study/study1.java
@@ -3,15 +3,35 @@ package study;
 public class study1 {
 
 	
+	//[변수]
+	//프로그램 작동 원리를 순서대로 적으시오.
 	
+	//[연산자]
+	//연산자 우선순위를 순서대로 적으시오.
+	//보기[단항연산자, 대입연산자, 삼항연산자, 이항연산자, 최우선연산자, 나열연산자]
 	public static void main(String[] args) {
 		
-		int value = 100;
-		int num = 150;
-		double doub = 15.6;
+		//1번 [변수]
+		System.out.println("키보드로 입력 받은값\n 파일로부터 읽어온 값 \n "
+				+ "네트워크로부터 전송 받은 값 \n"
+				+ " DB서버로부터 읽어온 값");
+		System.out.println("입력버퍼");
+		System.out.println("메모리에 기록");
+		System.out.println("CPU가 읽어서 계산 처리");
+		System.out.println("계산결과를 메모리로 보냄");
+		System.out.println("메모리에 결과값 기록함");
+		System.out.println("화면에 출력함" + "\n 파일에 저장함 \n 네트워크로 전송함 \n"
+				+ "DB서버에 기록함");
 		
-		System.out.println(value + num);
-
+		//2번 [연산자]
+		System.out.println("최우선연산자");
+		System.out.println("단항연산자");
+		System.out.println("이항연산자");
+		System.out.println("삼항연산자");
+		System.out.println("대입연산자");
+		System.out.println("나열연산자");
 	}
-
+	
+	
+	
 }
