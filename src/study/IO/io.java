29c59c9838681f3edff9f07980ec0f9b4a0d73e2
user <user@DESKTOP-TLIBF9G>
2020-07-13 02:15:12 +0900

    IO

---
+++ src/study/IO/io.java
@@ -0,0 +1,110 @@
+package study.IO;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class io {
+	//Bufferd를 사용하여 키보드로 입력을 받으시오.
+	//그리고 그내용을 출력하세요.
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("입력: ");
+		try {
+			
+			String str = br.readLine();
+			System.out.println(str);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
